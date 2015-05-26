13327c6e955411c67abd4c40b459c175b0df4a29
egialui <luigi.giancardi@ericsson.com>
2015-05-26 11:32:46 +0200

    Project Archetype created

---
+++ src/main/java/com/wildboar/smartcalendar/App.java
@@ -0,0 +1,13 @@
+package com.wildboar.smartcalendar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
